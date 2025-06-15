833b030589ad3707e26c2a9a2e0b32f6d0c7d6d5
Youssef <119854538+BseProg06@users.noreply.github.com>
2025-06-15 17:50:30 +0300

    update

---
+++ API/Controllers/LectureController.cs
@@ -0,0 +1,78 @@
+﻿using API.DTOs;
+using AutoMapper;
+using Core;
+using Core.Entities;
+using Core.Repositories.Contract;
+using Core.Specifications.SubjectsSpecifications;
+using Core.Specifications.SubjectsSpecParamsSpecifications;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Project.APIS.Erorrs;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Controllers
+{
+    public class LectureController : BaseApiController
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public LectureController(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+        [HttpGet("GetAllLecture")]   //Lecture
+        public async Task<ActionResult<IReadOnlyList<LectureDTO>>> GetAllSubjects()
+        {
+            var Lectures = await _unitOfWork.Repository<Lecture_S>().GetAll();
+            var data = _mapper.Map<IReadOnlyList<Lecture_S>, IReadOnlyList<LectureDTO>>(Lectures);
+            return Ok(data); //200
+        } 
+        [HttpPost("Add_OR_UpdateLecture")]
+        public async Task<ActionResult<Lecture_S>> AddSubject(LectureDTO Lecture_S)
+        { 
+
+            var mappedLecture_S = new Lecture_S
+            {
+                ID = Lecture_S.ID,
+                Lecture_Name = Lecture_S.Lecture_Name,
+                LectureDate = Lecture_S.LectureDate,
+                Sub_ID = Lecture_S.Sub_ID, 
+                Degree = Lecture_S.Degree
+            };
+            if (mappedLecture_S.ID != 0)
+            {
+                var data = await _unitOfWork.Repository<Lecture_S>().UpdateAsync(mappedLecture_S);
+                if (data is null) return BadRequest(new ApiResponse(400));
+                await _unitOfWork.CompleteAsync();
+                return Ok(data);
+            }
+            else
+            {
+                mappedLecture_S.ID = 0;
+                var data = await _unitOfWork.Repository<Lecture_S>().AddAsync(mappedLecture_S);
+                if (data is null) return BadRequest(new ApiResponse(400));
+                await _unitOfWork.CompleteAsync();
+                return Ok(data);
+            }
+
+
+        }
+        [HttpDelete("DeleteLecture")]
+        public async Task DeleteLecture_S(int id)
+        {
+            var Subject = await _unitOfWork.Repository<Lecture_S>().GetById(id);
+            if (Subject is not null)
+            {
+                _unitOfWork.Repository<Lecture_S>().Delete(Subject);
+                await _unitOfWork.CompleteAsync();
+            }
+            else
+            {
+                NotFound(new ApiResponse(404));// 404
+            }
+
+        }
+    }
+}
--- API/DTOs/LectureDTO.cs
+++ API/DTOs/LectureDTO.cs
@@ -5,11 +5,7 @@ namespace API.DTOs
     public class LectureDTO
     {
         public int ID { get; set; }
-        public string Sub_Name { get; set; } 
-        public int St_ID { get; set; }
-        public string Students { get; set; }
-        public int Room_ID { get; set; }
-        public string Rooms { get; set; }
+        public string Lecture_Name { get; set; }  
         public int Sub_ID { get; set; }
         public string Subjects { get; set; }
         public DateTime LectureDate { get; set; }
